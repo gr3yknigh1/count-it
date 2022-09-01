b65fa0bfd4411f67a53427f42b70b61a19f36fea
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-01 22:17:09 +0300

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+export default function App(): JSX.Element {
+  return (
+    <>
+      <header>
+        <h1>count-it</h1>
+      </header>
+      <main className="App">
+        Page content
+      </main>
+    </>
+  );
+}
