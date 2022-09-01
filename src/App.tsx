2679f162915f688af019a9934d49c00d915a0048
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-01 22:51:04 +0300

    Embed hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,76 @@
+import {
+  useCallback,
+  useState,
+  FormEvent,
+  ChangeEvent
+} from "react";
 
 function CardForm(): JSX.Element {
+
+  const [label, setLabel] = useState("");
+
+  const onCardFormSubmit = useCallback((event: FormEvent) => {
+    event.preventDefault();
+    console.log("Submit card");
+  }, []);
+
+  const onCardInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setLabel(event.target.value);
+  }, [setLabel]);
+
   return (
-    <form className="card-form">
-      <input className="card-form-input" type="text" placeholder="Name of new count"></input>
-      <button className="card-form-submit">new count</button>
+    <form
+      className="card-form"
+      onSubmit={ onCardFormSubmit }
+    >
+      <input
+        className="card-form-input"
+        type="text"
+        placeholder="Name of new count"
+        onChange={ onCardInputChange }
+        value={ label }
+      ></input>
+      <button
+        className="card-form-submit"
+        onSubmit={ onCardFormSubmit }
+      >
+        new count
+      </button>
     </form>
   );
 }
 
-
 function Card(): JSX.Element {
+
+  const openMenu = useCallback(() => {
+    console.log("Open menu");
+  }, []);
+
+  const changeCountValue = useCallback((delta: Number) => {
+    console.log(`change value by ${delta}`);
+  }, []);
+
   return (
-    <table className="card">
-      <tbody>
-        <tr>
-          <button className="card-menu">*</button>
-          <span className="card-label">card name</span>
-        </tr>
-        <tr>
-          <span className="card-count">0</span>
-        </tr>
-        <tr>
-          <td>
-            <button className="card-button card-button-plus">+</button>
-          </td>
-          <td>
-            <button className="card-button card-button-minus">-</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="card">
+      <button className="card-menu" onClick={ openMenu }>*</button>
+      <span className="card-label">card name</span>
+      <span className="card-count">0</span>
+      <button
+        className="card-button card-button-plus"
+        onClick={ () => { changeCountValue(+1); } }
+      >
+        +1
+      </button>
+      <button
+        className="card-button card-button-minus"
+        onClick={ () => { changeCountValue(-1); } }
+      >
+        -1
+      </button>
+    </div>
   );
 }
-
 
 export default function App(): JSX.Element {
   return (
@@ -43,7 +81,9 @@ export default function App(): JSX.Element {
       <main className="app">
         <CardForm/>
         <div className="card-container">
-          <Card/>
+          <Card />
+          <Card />
+          <Card />
         </div>
       </main>
     </>
