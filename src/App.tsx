4650acbe578fbb90b83979b54d966ab83154a173
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-01 22:32:37 +0300

    Extract raw markcup to sep compnents

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,39 @@
 
+function CardForm(): JSX.Element {
+  return (
+    <form className="card-form">
+      <input className="card-form-input" type="text" placeholder="Name of new count"></input>
+      <button className="card-form-submit">new count</button>
+    </form>
+  );
+}
+
+
+function Card(): JSX.Element {
+  return (
+    <table className="card">
+      <tbody>
+        <tr>
+          <button className="card-menu">*</button>
+          <span className="card-label">card name</span>
+        </tr>
+        <tr>
+          <span className="card-count">0</span>
+        </tr>
+        <tr>
+          <td>
+            <button className="card-button card-button-plus">+</button>
+          </td>
+          <td>
+            <button className="card-button card-button-minus">-</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+
 export default function App(): JSX.Element {
   return (
     <>
@@ -6,30 +41,9 @@ export default function App(): JSX.Element {
         <h1>count-it</h1>
       </header>
       <main className="app">
-        <form>
-          <input className="card-form-input" type="text" placeholder="Name of new count"></input>
-          <button className="card-form-submit">new count</button>
-        </form>
+        <CardForm/>
         <div className="card-container">
-          <table className="card">
-            <tbody>
-              <tr>
-                <button className="card-menu">*</button>
-                <span className="card-label">card name</span>
-              </tr>
-              <tr>
-                <span className="card-count">0</span>
-              </tr>
-              <tr>
-                <td>
-                  <button className="card-button card-button-plus">+</button>
-                </td>
-                <td>
-                  <button className="card-button card-button-minus">-</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <Card/>
         </div>
       </main>
     </>
