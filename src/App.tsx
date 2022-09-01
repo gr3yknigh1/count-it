49f322ffdc0575c84e0e195f2e49bb110a434651
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-09-01 22:28:12 +0300

    Add raw markcup

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,32 @@ export default function App(): JSX.Element {
       <header>
         <h1>count-it</h1>
       </header>
-      <main className="App">
-        Page content
+      <main className="app">
+        <form>
+          <input className="card-form-input" type="text" placeholder="Name of new count"></input>
+          <button className="card-form-submit">new count</button>
+        </form>
+        <div className="card-container">
+          <table className="card">
+            <tbody>
+              <tr>
+                <button className="card-menu">*</button>
+                <span className="card-label">card name</span>
+              </tr>
+              <tr>
+                <span className="card-count">0</span>
+              </tr>
+              <tr>
+                <td>
+                  <button className="card-button card-button-plus">+</button>
+                </td>
+                <td>
+                  <button className="card-button card-button-minus">-</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </>
   );
